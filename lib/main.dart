f1d332fbae61eb0e05ffe78212d4589438655b49
mogmet <mogmet@gmail.com>
2021-07-29 12:25:50 +0900

    [wip] 処理し忘れ

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -18,36 +19,29 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatefulWidget {
+class MyHomePageControllerState {
+  MyHomePageControllerState({required this.title, this.counter = 0});
+  String title;
+  int counter;
+}
+
+class MyHomePageController extends StateNotifier<MyHomePageControllerState> {
+  MyHomePageController(String title): super(MyHomePageControllerState(title: title)) {
+    state.counter = 10;
+}
+  static final page1CtrlProvider = StateNotifierProvider.family autoDispose((ref) => MyHomePageController(''));
+}
+
+class MyHomePage extends ConsumerWidget {
   MyHomePage({Key? key, required this.title}) : super(key: key);
   final String title;
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    setState(() {
-      _counter = 10; // 初期化処理！
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, ScopedReader watch) {
+    final counter = watch()
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text(context.read(provider)),
       ),
       body: Center(
         child: Column(
@@ -69,5 +63,51 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
-  }
+//   }
+// }
+//
+// class _MyHomePageState extends State<MyHomePage> {
+//   int _counter = 0;
+//
+//   void _incrementCounter() {
+//     setState(() {
+//       _counter++;
+//     });
+//   }
+//
+//   @override
+//   void initState() {
+//     super.initState();
+//     setState(() {
+//       _counter = 10; // 初期化処理！
+//     });
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text(widget.title),
+//       ),
+//       body: Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: <Widget>[
+//             Text(
+//               'You have pushed the button this many times:',
+//             ),
+//             Text(
+//               '$_counter',
+//               style: Theme.of(context).textTheme.headline4,
+//             ),
+//           ],
+//         ),
+//       ),
+//       floatingActionButton: FloatingActionButton(
+//         onPressed: _incrementCounter,
+//         tooltip: 'Increment',
+//         child: Icon(Icons.add),
+//       ), // This trailing comma makes auto-formatting nicer for build methods.
+//     );
+//   }
 }
